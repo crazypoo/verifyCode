01b48d0438c67f94953796f6a7fb812aa8ce7c89
crazypoo <273277355@qq.com>
2014-04-14 14:23:02 +0800

    Initial Commit

---
+++ Code/PooAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PooAppDelegate.h
+//  Code
+//
+//  Created by crazypoo on 14-4-14.
+//  Copyright (c) 2014年 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PooAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Code/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Code
+//
+//  Created by crazypoo on 14-4-14.
+//  Copyright (c) 2014年 crazypoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PooAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PooAppDelegate class]));
+    }
+}
